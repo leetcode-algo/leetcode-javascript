ec8b9849e9a192d1126739dde8bd26e0c1002a8c
Ethan Yu <sohbmpethan@gmail.com>
2021-01-15 10:54:21 +0900

    1281-subtract-the-product-and-sum-of-digits-of-an-integer

---
+++ math/0.8/1281-subtract-the-product-and-sum-of-digits-of-an-integer/index.js
@@ -0,0 +1,39 @@
+/*
+Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+
+Example 1:
+  Input: n = 234
+  Output: 15
+  Explanation:
+  Product of digits = 2 * 3 * 4 = 24
+  Sum of digits = 2 + 3 + 4 = 9
+  Result = 24 - 9 = 15
+
+Example 2:
+  Input: n = 4421
+  Output: 21
+Explanation:
+  Product of digits = 4 * 4 * 2 * 1 = 32
+  Sum of digits = 4 + 4 + 2 + 1 = 11
+  Result = 32 - 11 = 21
+
+Constraints:
+  1 <= n <= 10^5
+*/
+/*
+ * @param {number} n
+ * @return {number}
+ */
+const subtractProductAndSum = function(n) {
+  let product = 1;
+  let sum = 0;
+  while (n) {
+    const tmp = n % 10;
+    n = Math.floor(n / 10);
+    product *= tmp;
+    sum += tmp;
+  }
+  return product - sum;
+};
+
+console.log(subtractProductAndSum(4421));

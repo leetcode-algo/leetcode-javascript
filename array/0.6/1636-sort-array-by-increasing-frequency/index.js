e4a86bb11e841edd97f143203c97ca0317494dc6
Ethan Yu <sohbmpethan@gmail.com>
2021-01-27 23:31:10 +0900

    1636-sort-array-by-increasing-frequency

---
+++ array/0.6/1636-sort-array-by-increasing-frequency/index.js
@@ -0,0 +1,16 @@
+/*
+
+*/
+/*
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+const frequencySort = function(nums) {
+  const map = new Map();
+  for (const n of nums) {
+    map.set(n, map.get(n) + 1 || 1);
+  }
+  return nums.sort((a, b) => map.get(a) - map.get(b) || b - a);
+};
+
+console.log(frequencySort());

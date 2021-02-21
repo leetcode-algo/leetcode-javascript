dca00d032d71440a9d4dcf9e0315dee1199e60c8
Ethan Yu <sohbmpethan@gmail.com>
2021-02-21 11:18:48 +0900

    350-intersection-of-two-arrays-ii

---
+++ sort/0.5/350-intersection-of-two-arrays-ii/index.js
@@ -0,0 +1,42 @@
+/*
+Given two arrays, write a function to compute their intersection.
+
+Example 1:
+  Input: nums1 = [1,2,2,1], nums2 = [2,2]
+  Output: [2,2]
+
+Example 2:
+  Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+  Output: [4,9]
+
+Note:
+  Each element in the result should appear as many times as it shows in both arrays.
+  The result can be in any order.
+Follow up:
+  What if the given array is already sorted? How would you optimize your algorithm?
+  What if nums1's size is small compared to nums2's size? Which algorithm is better?
+  What if elements of nums2 are stored on disk, and the memory is limited such that you cannot load all elements into the memory at once?
+*/
+/*
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+const intersect = function(nums1, nums2) {
+  const map = new Map();
+  const result = [];
+  for (let i = 0; i < nums1.length; ++i) {
+    if (!map.get(nums1[i])) map.set(nums1[i], 1);
+    else map.set(nums1[i], map.get(nums1[i]) + 1);
+  }
+  for (let i = 0; i < nums2.length; ++i) {
+    if (map.get(nums2[i])) {
+      result.push(nums2[i]);
+      map.set(nums2[i], map.get(nums2[i]) - 1);
+    }
+  }
+  return result;
+};
+
+// console.log(intersect([1, 2, 2, 1], [2, 2]));
+console.log(intersect([1, 2, 2, 1], [1, 1]));
